a29eb75bcfdeafdf7ba445eb9322ac638ca09bfc
unknown <lior1990@gmail.com>
2020-10-23 14:13:34 +0300

    code from MSGAN

--- train_image.py
+++ train_image.py
@@ -166,18 +166,21 @@ def train(opt, netG):
             rec_vae_loss = opt.rec_loss(generated, real) + opt.rec_loss(generated_vae, real_zero)
             vae_loss = opt.rec_weight * rec_vae_loss + features_loss * opt.features_loss_weight
 
-            # only for batch size 2
-            b0, b1 = real_zero[0].unsqueeze(dim=0), real_zero[1].unsqueeze(dim=0)
+            vae_eps1 = utils.generate_noise(size=opt.Z_init_size, device=opt.device) * opt.vae_noise_weight
+            vae_eps2 = utils.generate_noise(size=opt.Z_init_size, device=opt.device) * opt.vae_noise_weight
 
-            if (b0 != b1).any().item():
-                vae_eps = utils.generate_noise(size=opt.Z_init_size, device=opt.device) * opt.vae_noise_weight
-                _, generated_vae1, _, mu = G_curr(real_zero, opt.Noise_Amps, mode="rand", vae_eps=vae_eps)
-                mu1, mu2 = mu[0].unsqueeze(dim=0), mu[1].unsqueeze(dim=0)
+            _, generated_vae1, _, z_vae1 = G_curr(real_zero, opt.Noise_Amps, mode="rand", vae_eps=vae_eps1)
+            _, generated_vae2, _, z_vae2 = G_curr(real_zero, opt.Noise_Amps, mode="rand", vae_eps=vae_eps2)
 
-                diversity_loss = -diversity_loss_fn(mu1, mu2)
-                total_loss += diversity_loss * opt.diversity_loss_weight
+            # diversity_loss = (diversity_loss_fn(z_vae1, z_vae2) / (diversity_loss_fn(generated_vae1, generated_vae2) + 0.001)) * opt.diversity_loss_weight
 
-            total_loss += vae_loss
+            # objective: maximize the ratio between generated images and their corresponding latent space vectors
+            lz = torch.mean(torch.abs(generated_vae2 - generated_vae1)) / torch.mean(torch.abs(z_vae2 - z_vae1)) * opt.diversity_loss_weight
+            eps = 1 * 1e-5
+            # objective: maximize lz
+            diversity_loss = 1 / (lz + eps)
+
+            total_loss += vae_loss + diversity_loss
         else:
             ############################
             # (2) Update D network: maximize D(x) + D(G(z))
