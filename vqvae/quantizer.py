b8e725e917b697fda70c973cb1d39435178761be
unknown <lior1990@gmail.com>
2021-03-07 19:05:14 +0200

    restore vq_rand

--- vqvae/quantizer.py
+++ vqvae/quantizer.py
@@ -43,23 +43,14 @@ class VectorQuantizer(nn.Module):
         """
         # reshape z -> (batch, height, width, channel) and flatten
         z = z.permute(0, 2, 3, 1).contiguous()
-        z_flattened = z.view(-1, self.e_dim)
-        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
 
-        d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
-            torch.sum(self.embedding.weight**2, dim=1) - 2 * \
-            torch.matmul(z_flattened, self.embedding.weight.t())
+        min_encoding_indices = self.find_closest_encodings_indices(z)
 
-        # find closest encodings
-        min_encoding_indices = torch.argmin(d, dim=1).unsqueeze(1)
-        if mode == "rand":
+        if mode in ["rand", "vq_rand"]:
             min_encoding_indices = (min_encoding_indices + torch.randint_like(min_encoding_indices, 0, self.n_e)) % self.n_e
-        min_encodings = torch.zeros(
-            min_encoding_indices.shape[0], self.n_e).to(device)
-        min_encodings.scatter_(1, min_encoding_indices, 1)
 
-        # get quantized latent vectors
-        z_q = torch.matmul(min_encodings, self.embedding.weight).view(z.shape)
+        min_encodings, z_q = self.get_quantized_embeddings(min_encoding_indices)
+        z_q = z_q.view(z.shape)
 
         # compute loss for embedding
         loss = torch.mean((z_q.detach()-z)**2) + self.beta * \
@@ -76,3 +67,39 @@ class VectorQuantizer(nn.Module):
         z_q = z_q.permute(0, 3, 1, 2).contiguous()
 
         return loss, z_q, perplexity, min_encodings, min_encoding_indices
+
+    def find_closest_encodings_indices(self, z):
+        z_flattened = z.view(-1, self.e_dim)
+        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
+
+        d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
+            torch.sum(self.embedding.weight**2, dim=1) - 2 * \
+            torch.matmul(z_flattened, self.embedding.weight.t())
+
+        # find closest encodings
+        min_encoding_indices = torch.argmin(d, dim=1).unsqueeze(1)
+        return min_encoding_indices
+
+    def get_quantized_embeddings(self, encoding_indices):
+        min_encodings = torch.zeros(encoding_indices.shape[0], self.n_e).to(device)
+        min_encodings.scatter_(1, encoding_indices, 1)
+
+        # get quantized latent vectors
+        z_q = torch.matmul(min_encodings, self.embedding.weight)
+
+        return min_encodings, z_q
+
+"""
+todos:
+
+exps:
+1. run with scale factor 0.85
+2. try different ref-stop-scale 
+3. change noise for reference dataset
+4. decrease number of downsampling
+
+
+checks:
+influence of random index vs random noise
+pixel-cnn?
+"""
